e971c5dddb6a8e8442a6485ca9a31766d009f66f
linjiazeng <tobias_2016@163.com>
2019-04-24 16:36:00 +0800

    `dsa`

--- src/com/tobias/Algorithm_forth/captrer_one/BinarySearch.java
+++ src/com/tobias/Algorithm_forth/captrer_one/BinarySearch.java
@@ -5,8 +5,7 @@ public class BinarySearch {
   public static void main(String[] args) {
 
     System.out.println(new BinarySearch().findKeyByBinarySearch(4, new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9}));
-
-  }
+   }
 
   public int findKeyByBinarySearch(int key, int[] origin) {
     int low = 0;
