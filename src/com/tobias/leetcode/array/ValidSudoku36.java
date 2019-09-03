06fd42e31915283b2ad81dbdf48fb67b6b4f8343
Tobias <2289975919@qq.com>
2019-09-03 09:03:20 +0800

    ` `

---
+++ src/com/tobias/leetcode/array/ValidSudoku36.java
@@ -0,0 +1,189 @@
+package com.tobias.leetcode.array;
+
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Determine if a 9x9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+ *
+ * Each row must contain the digits 1-9 without repetition.
+ * Each column must contain the digits 1-9 without repetition.
+ * Each of the 9 3x3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+ *
+ * A partially filled sudoku which is valid.
+ *
+ * The Sudoku board could be partially filled, where empty cells are filled with the character '.'.
+ *
+ * Example 1:
+ *
+ * Input:
+ * [
+ *   ["5","3",".",".","7",".",".",".","."],
+ *   ["6",".",".","1","9","5",".",".","."],
+ *   [".","9","8",".",".",".",".","6","."],
+ *   ["8",".",".",".","6",".",".",".","3"],
+ *   ["4",".",".","8",".","3",".",".","1"],
+ *   ["7",".",".",".","2",".",".",".","6"],
+ *   [".","6",".",".",".",".","2","8","."],
+ *   [".",".",".","4","1","9",".",".","5"],
+ *   [".",".",".",".","8",".",".","7","9"]
+ * ]
+ * Output: true
+ * Example 2:
+ *
+ * Input:
+ * [
+ *   ["8","3",".",".","7",".",".",".","."],
+ *   ["6",".",".","1","9","5",".",".","."],
+ *   [".","9","8",".",".",".",".","6","."],
+ *   ["8",".",".",".","6",".",".",".","3"],
+ *   ["4",".",".","8",".","3",".",".","1"],
+ *   ["7",".",".",".","2",".",".",".","6"],
+ *   [".","6",".",".",".",".","2","8","."],
+ *   [".",".",".","4","1","9",".",".","5"],
+ *   [".",".",".",".","8",".",".","7","9"]
+ * ]
+ * Output: false
+ * Explanation: Same as Example 1, except with the 5 in the top left corner being
+ *     modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+ * Note:
+ *
+ * A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+ * Only the filled cells need to be validated according to the mentioned rules.
+ * The given board contain only digits 1-9 and the character '.'.
+ * The given board size is always 9x9.
+ */
+public class ValidSudoku36 {
+
+  public boolean isValidSudoku(char[][] board) {
+    int [] vset = new int [9];
+    int [] hset = new int [9];
+    int [] bckt = new int [9];
+    int idx = 0;
+    for (int i = 0; i < 9; i++) {
+      for (int j = 0; j < 9; j++) {
+        if (board[i][j] != '.') {
+          idx = 1 << (board[i][j] - '0') ;
+          if ((hset[i] & idx) > 0 ||
+              (vset[j] & idx) > 0 ||
+              (bckt[(i / 3) * 3 + j / 3] & idx) > 0) return false;
+          hset[i] |= idx;
+          vset[j] |= idx;
+          bckt[(i / 3) * 3 + j / 3] |= idx;
+        }
+      }
+    }
+    return true;
+  }
+
+
+  public boolean bestResolveIsValidSudoku(char[][] board) {
+    Set<Object> seen = new HashSet<>();
+    for (int i = 0; i < board.length; i++) {
+      for (int j = 0; j < board[i].length; j++) {
+        if (board[i][j] != '.') {
+          String str = "(" + board[i][j] + ")";
+          if (!seen.add(i + str) || !seen.add(str + j) || !seen.add(i / 3 + str + j / 3)) {
+            return false;
+          }
+        }
+      }
+    }
+
+    return true;
+  }
+
+
+    public boolean isValidSudokuByViolence(char[][] board) {
+    for (char[] rows : board) {
+      if (!isValidRow(rows)) {
+        return false;
+      }
+    }
+    for (int i = 0; i < board.length; i++) {
+      if (!isValidCol(i, board)) {
+        return false;
+      }
+    }
+
+    for (int i = 0; i < board.length; i += 3) {
+      for (int j = 0; j < board[i].length; j += 3) {
+        if (!isValidSubTable(i, j, board)) {
+          return false;
+        }
+      }
+    }
+
+    return true;
+  }
+
+  private boolean isValidSubTable(int row, int col, char[][] board) {
+    Map<Character, Integer> map = new HashMap<>();
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 3; j++) {
+        char c = board[row + i][col + j];
+        if (c != '.') {
+          if (map.getOrDefault(c, 0) != 0) {
+            return false;
+          } else {
+            map.put(c, 1);
+          }
+        }
+      }
+    }
+    return true;
+  }
+
+
+    private boolean isValidCol(int col, char[][] board) {
+    Map<Character, Integer> map = new HashMap<>();
+      for (char[] chars : board) {
+        if (chars[col] != '.') {
+          if (map.getOrDefault(chars[col], 0) != 0) {
+            return false;
+          } else {
+            map.put(chars[col], 1);
+          }
+        }
+      }
+    return true;
+  }
+
+
+  private boolean isValidRow(char[] boardRow) {
+    Map<Character, Integer> map = new HashMap<>();
+    for (char c : boardRow) {
+      if (c != '.') {
+        if (map.getOrDefault(c, 0) != 0) {
+          return false;
+        } else {
+          map.put(c, 1);
+        }
+      }
+    }
+    return true;
+  }
+
+
+  public static void main(String[] args) {
+    ValidSudoku36 validSudoku36 = new ValidSudoku36();
+    char[][] temp = new char[][]{
+        {'5','3','.','.','7','.','.','.','.'},
+        {'6','.','.','1','9','5','.','.','.'},
+        {'.','9','8','.','.','.','.','6','.'},
+        {'8','.','.','.','6','.','.','.','3'},
+        {'4','.','.','8','.','3','.','.','1'},
+        {'7','.','.','.','2','.','.','.','6'},
+        {'.','6','.','.','.','.','2','8','.'},
+        {'.','.','.','4','1','9','.','.','5'},
+        {'.','.','.','.','8','.','.','7','9'},
+  };
+
+    System.out.println(validSudoku36.isValidSudoku(temp));
+  }
+
+
+}
---
+++ src/com/tobias/leetcode/string/MergeSortedArray88.java
@@ -0,0 +1,101 @@
+package com.tobias.leetcode.string;
+
+
+import java.util.Arrays;
+
+/**
+ * Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+ *
+ * Note:
+ *
+ * The number of elements initialized in nums1 and nums2 are m and n respectively. You may assume
+ * that nums1 has enough space (size that is greater or equal to m + n) to hold additional elements
+ * from nums2. Example:
+ *
+ * Input: nums1 = [1,2,3,4,0,0], m = 3 nums2 = [2,5,6],       n = 3
+ *
+ * Output: [1,2,2,3,5,6]
+ */
+public class MergeSortedArray88 {
+
+  public void mergeByDirect(int[] nums1, int m, int[] nums2, int n) {
+    if (n == 0) {
+      return;
+    }
+
+    int i = 0, j = 0;
+
+    while (j < n) {
+      if (m + j == i) {
+        nums1[i++] = nums2[j++];
+        continue;
+      }
+      if (nums1[i] >= nums2[j]) {
+        if (m + j - i >= 0)
+          System.arraycopy(nums1, i, nums1, i + 1, m + j - i);
+        nums1[i++] = nums2[j++];
+      } else {
+        i++;
+      }
+    }
+
+    System.out.println(Arrays.toString(nums1));
+  }
+
+  public void mergeByCombine(int[] nums1, int m, int[] nums2, int n) {
+    if (n == 0) {
+      return;
+    }
+    if (m >= 0)
+      System.arraycopy(nums1, 0, nums1, n, m);
+    int i = n, j = 0, k = 0;
+    while (j < n) {
+      if (n + m == i) {
+        while (j < n) {
+          nums1[k++] = nums2[j++];
+        }
+        System.out.println(Arrays.toString(nums1));
+        return;
+      }
+
+      if (nums1[i] < nums2[j]) {
+        nums1[k++] = nums1[i++];
+      } else {
+        nums1[k++] = nums2[j++];
+      }
+    }
+
+  }
+
+
+  public void merge3(int[] nums1, int m, int[] nums2, int n) {
+    int i = m - 1;
+    int j = n - 1;
+    int k = m + n - 1;
+    while (j >= 0) {
+      if (i < 0) {
+        while (j >= 0) {
+          nums1[k--] = nums2[j--];
+        }
+        return;
+      }
+      //哪个数大就对应的添加哪个数。
+      if (nums1[i] > nums2[j]) {
+        nums1[k--] = nums1[i--];
+      } else {
+        nums1[k--] = nums2[j--];
+      }
+    }
+  }
+
+
+
+
+  public static void main(String[] args) {
+    MergeSortedArray88 mergeSortedArray88 = new MergeSortedArray88();
+//    mergeSortedArray88.mergeByDirect(new int[]{1, 2, 3, 4, 0, 0, 0, 0}, 4, new int[]{1, 2, 5, 6}, 4);
+//    mergeSortedArray88.mergeByDirect(new int[]{1, 2, 3, 4, 0, 0, 0}, 4, new int[]{2, 5, 6}, 3);
+    mergeSortedArray88.merge3(new int[]{1, 2 ,3, 0, 0, 0, 0}, 3, new int[]{2, 5, 6, 7}, 4);
+    mergeSortedArray88.merge3(new int[]{4,0,0,0,0,0}, 1, new int[]{1,2,3,5,6}, 5);
+  }
+}
