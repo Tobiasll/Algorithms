05f00a3418e41588a8d22a1b824e5c2e95f42237
Tobias <2289975919@qq.com>
2019-09-20 15:41:35 +0800

    ` `

--- src/com/tobias/leetcode/array/SubsetsII90.java
+++ src/com/tobias/leetcode/array/SubsetsII90.java
@@ -24,7 +24,52 @@ import java.util.List;
  */
 public class SubsetsII90 {
 
-  public List<List<Integer>> subsetsWithDup(int[] nums) {
+  public List<List<Integer>> subsetsWithDupByForeach2(int[] nums) {
+    List<List<Integer>> result = new ArrayList<>();
+    Arrays.sort(nums);
+    result.add(new ArrayList<>());
+    for (int i = 0; i < nums.length; i++) {
+      int count = 0;
+      while (i + 1 < nums.length && nums[i] == nums[i + 1]) {
+        count++;
+        i++;
+      }
+      List<List<Integer>> tempList = new ArrayList<>();
+      for (List<Integer> list : result) {
+        List<Integer> insideList = new ArrayList<>(list);
+        for (int c = 0; c <= count; c++) {
+          insideList.add(nums[i]);
+          tempList.add(new ArrayList<>(insideList));
+        }
+      }
+      result.addAll(tempList);
+    }
+
+    return result;
+  }
+
+    public List<List<Integer>> subsetsWithDupByForeach1(int[] nums) {
+    List<List<Integer>> result = new ArrayList<>();
+    Arrays.sort(nums);
+    result.add(new ArrayList<>());
+    int newSolveValue = result.size();
+    for (int i = 0; i < nums.length; i++) {
+      List<List<Integer>> tempList = new ArrayList<>();
+      for (int j = 0; j < result.size(); j++) {
+        if (i > 0 && nums[i] == nums[i - 1] && j < newSolveValue) {
+          continue;
+        }
+        List<Integer> insideList = new ArrayList<>(result.get(j));
+        insideList.add(nums[i]);
+        tempList.add(insideList);
+      }
+      newSolveValue = result.size();
+      result.addAll(tempList);
+    }
+    return result;
+  }
+
+    public List<List<Integer>> subsetsWithDupByBacktrack(int[] nums) {
     List<List<Integer>> result = new ArrayList<>();
     Arrays.sort(nums);
     backtrack(nums, result, new ArrayList<>(), 0);
@@ -48,7 +93,8 @@ public class SubsetsII90 {
 
   public static void main(String[] args) {
     SubsetsII90 subsetsII90 = new SubsetsII90();
-    List<List<Integer>> list = subsetsII90.subsetsWithDup(new int[]{1, 2, 2});
+//    List<List<Integer>> list = subsetsII90.subsetsWithDupByForeach2(new int[]{1, 2, 2});
+    List<List<Integer>> list = subsetsII90.subsetsWithDupByForeach2(new int[]{4,4,4,1,4});
     for (List<Integer> integers : list) {
       System.out.println(integers);
     }
