da0f3d1379e2a02083376bd8c2695b6d28b18857
Tobaisll <2289975919@qq.com>
2019-10-09 10:20:59 +0800

    ` `

--- src/com/tobias/leetcode/linked_list/MergeKSortedLists23.java
+++ src/com/tobias/leetcode/linked_list/MergeKSortedLists23.java
@@ -3,6 +3,8 @@ package com.tobias.leetcode.linked_list;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 /**
  * Merge k sorted linked lists and return it as one sorted list. Analyze and describe its
@@ -14,9 +16,74 @@ import java.util.List;
  */
 public class MergeKSortedLists23 {
 
-  public ListNode mergeKLists3(ListNode[] lists) {
-    ListNode result = new ListNode(0);
+  public ListNode mergeKLists4(ListNode[] lists) {
+    if (lists == null || lists.length == 0) {
+      return null;
+    }
+    if (lists.length == 1) {
+      return lists[0];
+    }
+    int n = 1;
+    while (n < lists.length) {
+      // 第一次合并为0, 1、2，3、4，5、6，7
+      // 第二次合并为0，2、4，6
+      // 第三次合并为0，4、8，12
+      for (int i = 0; i + n < lists.length; i = i + n * 2) {
+        lists[i] = mergeKLists(lists[i], lists[i + n]);
+      }
+      n *= 2;
+    }
 
+    return lists[0];
+  }
+
+  private ListNode mergeKLists(ListNode node1, ListNode node2) {
+    ListNode result = new ListNode(0);
+    ListNode root = result;
+    // 不可以使用 || 或条件，因为其中一个会为null
+    while (node1 != null && node2 != null) {
+      if (node1.val < node2.val) {
+        root.next = new ListNode(node1.val);
+        node1 = node1.next;
+      } else {
+        root.next = new ListNode(node2.val);
+        node2 = node2.next;
+      }
+      root = root.next;
+    }
+
+    if (node1 != null) {
+      root.next = node1;
+    }
+    if (node2 != null) {
+      root.next = node2;
+    }
+    return result.next;
+  }
+
+  /**
+     * Runtime: 38 ms, faster than 22.41% of Java online submissions for Merge k Sorted Lists.
+     * Memory Usage: 38 MB, less than 95.63% of Java online submissions for Merge k Sorted Lists.
+     */
+  public ListNode mergeKLists3(ListNode[] lists) {
+
+    ListNode result = new ListNode(0);
+    Queue<ListNode> queue = new PriorityQueue<>(Comparator.comparingInt(o -> o.val));
+    for (ListNode list : lists) {
+      // list 为空会报错，优先队列的offer源码会监测是否为空，一旦为空会抛空指针异常，同时判空可以减少一丢丢时间复杂度（忽略不计）
+      if (list != null ) {
+        queue.add(list);
+      }
+    }
+    ListNode root = result;
+    while (!queue.isEmpty()) {
+      root.next = queue.poll();
+      root = root.next;
+      ListNode next = root.next;
+      if (next != null) {
+        queue.add(next);
+      }
+    }
     return result.next;
   }
 
@@ -94,12 +161,10 @@ public class MergeKSortedLists23 {
 
   public static void main(String[] args) {
     MergeKSortedLists23 mergeKSortedLists23 = new MergeKSortedLists23();
-    System.out
-        .println(mergeKSortedLists23.mergeKLists1(new ListNode[]{new ListNode(new int[]{1, 4, 5}),
+    System.out.println(mergeKSortedLists23.mergeKLists4(new ListNode[]{new ListNode(new int[]{1, 4, 5}),
             new ListNode(new int[]{1, 3, 4}), new ListNode(new int[]{2, 6})}));
-    System.out.println(mergeKSortedLists23.mergeKLists1(new ListNode[]{null, null}));
-    System.out.println(mergeKSortedLists23.mergeKLists1(
-        new ListNode[]{new ListNode(new int[]{1, 2, 2}), new ListNode(new int[]{1, 1, 2})}));
+    System.out.println(mergeKSortedLists23.mergeKLists4(new ListNode[]{null, null}));
+    System.out.println(mergeKSortedLists23.mergeKLists4(new ListNode[]{new ListNode(new int[]{1, 2, 2}), new ListNode(new int[]{1, 1, 2})}));
 
   }
 }
