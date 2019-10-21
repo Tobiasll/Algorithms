1fb970b99c7d2533a9faace9dd326bf43961fbd9
Tobaisll <2289975919@qq.com>
2019-10-21 09:51:01 +0800

    ` `

--- src/com/tobias/leetcode/other/PowerOfTwo231.java
+++ src/com/tobias/leetcode/other/PowerOfTwo231.java
@@ -22,6 +22,10 @@ package com.tobias.leetcode.other;
 public class PowerOfTwo231 {
 
   public boolean isPowerOfTwo(int n) {
+    return n > 0 && (n & (n - 1)) == 0;
+  }
+
+  public boolean isPowerOfTwo3(int n) {
     if (n == 0) {
       return false;
     }
