5762d2a53c4dcea6f5961064ff34e95c37d8bf51
Tobaisll <2289975919@qq.com>
2019-11-02 11:09:14 +0800

    ` `

---
+++ src/com/tobias/rudiment/graph/Dijkstra.java
@@ -0,0 +1,89 @@
+package com.tobias.rudiment.graph;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+public class Dijkstra {
+
+  static class Node{
+    char value;
+    int weight;
+
+    public Node() {
+    }
+
+    public Node(char value, int weight) {
+      this.value = value;
+      this.weight = weight;
+    }
+  }
+
+  private static Map<Character, List<Node>> graph = new HashMap<>();
+
+  static {
+    graph.put('A', Arrays.asList(new Node('B', 5), new Node('C', 1)));
+    graph.put('B', Arrays.asList(new Node('A', 5), new Node('C', 2), new Node('D', 1)));
+    graph.put('C', Arrays.asList(new Node('B', 2), new Node('A', 1),
+        new Node('D', 4), new Node('E', 8)));
+    graph.put('D', Arrays.asList(new Node('B', 1), new Node('C', 4),
+        new Node('E', 3), new Node('F', 6)));
+    graph.put('E', Arrays.asList(new Node('C', 8), new Node('D', 3)));
+    graph.put('F', Collections.singletonList(new Node('D', 6)));
+  }
+
+  public static Map<String, Object> dijkstra(char start, char end) {
+    PriorityQueue<Node> pq = new PriorityQueue<>(Comparator.comparingInt(o -> o.weight));
+    pq.add(new Node(start, 0));
+    boolean[] seen = new boolean[graph.size()];
+
+    char[] parent = new char[graph.size()];
+    parent[start - 'A'] = ' ';
+    int[] distance = new int[graph.size()];
+    for (char s : graph.keySet()) {
+      if (s != start) {
+        distance[s - 'A'] = Integer.MAX_VALUE;
+      }
+    }
+    while (!pq.isEmpty()) {
+      Node pair = pq.poll();
+      int dist = pair.weight;
+      char vertex = pair.value;
+      seen[vertex - 'A'] = true;
+      List<Node> nodes = graph.get(vertex);
+      for (Node node : nodes) {
+        if (!seen[node.value - 'A']) {
+          if (dist + node.weight < distance[node.value - 'A']) {
+            node.weight += dist;
+            pq.add(node);
+            parent[node.value - 'A'] =  vertex;
+            distance[node.value - 'A'] =  node.weight;
+          }
+        }
+      }
+
+    }
+    Map<String, Object> result = new HashMap<>(2);
+    result.put("mintPath", distance[end - 'A']);
+    StringBuilder sb = new StringBuilder();
+    while (end != ' ') {
+      sb.append(end).append(" -> ");
+      end = parent[end - 'A'];
+    }
+    result.put("path", sb.substring(0, sb.length() - 3).toString());
+    return result;
+  }
+
+  public static Map<String, Object> getMinPathAndPrintPath(char start, char end) {
+    return dijkstra(start, end);
+
+  }
+
+  public static void main(String[] args) {
+    System.out.println(getMinPathAndPrintPath('A', 'F'));
+  }
+}
---
+++ src/com/tobias/rudiment/graph/Test.java
@@ -0,0 +1,86 @@
+package com.tobias.rudiment.graph;
+
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.Stack;
+
+/**
+ *
+ */
+public class Test {
+
+  private static Map<String, List<String>> graph = new HashMap<>();
+  static {
+    graph.put("A", Arrays.asList("B", "C"));
+    graph.put("B", Arrays.asList("A", "C", "D"));
+    graph.put("C", Arrays.asList("A", "B", "D"));
+    graph.put("D", Arrays.asList("B", "C", "E", "F"));
+    graph.put("E", Arrays.asList("D", "C"));
+    graph.put("F", Collections.singletonList("D"));
+  }
+
+  public static Map<String, String> BFS(String start) {
+    LinkedList<String> queue = new LinkedList<>();
+    Set<String> set = new HashSet<>();
+    Map<String, String> map = new HashMap<>();
+    queue.add(start);
+    set.add(start);
+    map.put(start, null);
+    while (!queue.isEmpty()) {
+      String pollString = queue.poll();
+      List<String> list = graph.get(pollString);
+      for (String s : list) {
+        if (!set.contains(s)) {
+          queue.add(s);
+          set.add(s);
+          map.put(s, pollString);
+        }
+      }
+//      System.out.println(pollString);
+    }
+
+    return map;
+  }
+
+  public static void DFS(String start) {
+    Stack<String> stack = new Stack<>();
+    Set<String> set = new HashSet<>();
+    stack.add(start);
+    set.add(start);
+    while (!stack.isEmpty()) {
+      String pollString = stack.pop();
+      List<String> list = graph.get(pollString);
+      for (String s : list) {
+        if (!set.contains(s)) {
+          stack.add(s);
+          set.add(s);
+        }
+      }
+//      System.out.println(pollString);
+    }
+  }
+
+
+  public static int getMinPath(String start, String end) {
+    Map<String, String> map = BFS(start);
+    int count = 0;
+    while (end != null) {
+      System.out.println(end);
+      end = map.get(end);
+      count++;
+    }
+    return count;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(getMinPath("A", "F"));
+
+  }
+}
