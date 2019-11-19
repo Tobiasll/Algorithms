e61a82c64c979ee858d04ee71eede681162cff86
Tobaisll <2289975919@qq.com>
2019-11-19 14:04:55 +0800

    ` `

---
+++ src/com/tobias/leetcode/bst/S_114FlattenBinaryTreeToLinkedList.java
@@ -0,0 +1,62 @@
+package com.tobias.leetcode.bst;
+
+
+import com.tobias.rudiment.trie.BinaryTree;
+import com.tobias.rudiment.trie.BinaryTree.TreeNode;
+
+/**
+ *Given a binary tree, flatten it to a linked list in-place.
+ *
+ * For example, given the following tree:
+ *
+ *     1
+ *    / \
+ *   2   5
+ *  / \   \
+ * 3   4   6
+ * The flattened tree should look like:
+ *
+ * 1
+ *  \
+ *   2
+ *    \
+ *     3
+ *      \
+ *       4
+ *        \
+ *         5
+ *          \
+ *           6
+ */
+public class S_114FlattenBinaryTreeToLinkedList {
+
+  public TreeNode flatten(TreeNode root) {
+    TreeNode currentRoot = root;
+
+    while (currentRoot != null) {
+      if (currentRoot.left != null) {
+        TreeNode pre = currentRoot.left;
+        while (pre.right != null) {
+          pre = pre.right;
+        }
+        pre.right = currentRoot.right;
+        currentRoot.right = currentRoot.left;
+        currentRoot.left = null;
+      }
+      currentRoot = currentRoot.right;
+    }
+    return root;
+  }
+
+  public static void main(String[] args) {
+    BinaryTree binaryTree = new BinaryTree(new int[]{1, 5, 6});
+    binaryTree.getRoot().left = new TreeNode(2);
+    binaryTree.getRoot().left.left = new TreeNode(3);
+    binaryTree.getRoot().left.right = new TreeNode(4);
+    System.out.println(binaryTree);
+    S_114FlattenBinaryTreeToLinkedList flattenBinaryTreeToLinkedList = new S_114FlattenBinaryTreeToLinkedList();
+    binaryTree.setRoot(flattenBinaryTreeToLinkedList.flatten(binaryTree.getRoot()));
+    System.out.println(binaryTree);
+  }
+
+}
--- src/com/tobias/leetcode/bst/S_257BinaryTreePaths.java
+++ src/com/tobias/leetcode/bst/S_257BinaryTreePaths.java
@@ -57,7 +57,6 @@ public class S_257BinaryTreePaths {
         }
       }
     }
-
     return result;
   }
 
