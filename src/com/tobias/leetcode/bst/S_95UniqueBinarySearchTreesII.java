7e8da84adafd915a17db2b78ef965b53b4eae072
Tobaisll <2289975919@qq.com>
2019-11-21 18:02:05 +0800

    ` `

--- src/com/tobias/leetcode/bst/S_95UniqueBinarySearchTreesII.java
+++ src/com/tobias/leetcode/bst/S_95UniqueBinarySearchTreesII.java
@@ -31,7 +31,45 @@ import java.util.List;
  */
 public class S_95UniqueBinarySearchTreesII {
 
+
   public List<TreeNode> generateTrees(int n) {
+    ArrayList<TreeNode>[] dp = new ArrayList[n + 1];
+    dp[0] = new ArrayList<>();
+    if (n == 0) {
+      return dp[0];
+    }
+    dp[0].add(null);
+
+    for (int i = 1; i <= n; i++) {
+      dp[i] = new ArrayList<>();
+      for (int j = 1; j <= i; j++) {
+        ArrayList<TreeNode> leftTrees = dp[j - 1];
+        ArrayList<TreeNode> rightTrees = dp[i - j];
+        for (TreeNode leftTree : leftTrees) {
+          for (TreeNode rightTree : rightTrees) {
+
+            TreeNode treeNode = new TreeNode(j);
+            treeNode.left = leftTree;
+            treeNode.right = clone(rightTree, j);
+            dp[i].add(treeNode);
+          }
+        }
+      }
+    }
+    return dp[n];
+  }
+
+  private TreeNode clone(TreeNode root, int j) {
+    if (root == null) {
+      return null;
+    }
+    TreeNode treeNode = new TreeNode(root.val + j);
+    treeNode.left = clone(root.left, j);
+    treeNode.right = clone(root.right, j);
+    return treeNode;
+  }
+
+  public List<TreeNode> generateTreesByRecursive(int n) {
     return generateTrees(1, n);
   }
 
@@ -64,7 +102,7 @@ public class S_95UniqueBinarySearchTreesII {
 
   public static void main(String[] args) {
     S_95UniqueBinarySearchTreesII uniqueBinarySearchTreesII = new S_95UniqueBinarySearchTreesII();
-    List<TreeNode> treeNodes = uniqueBinarySearchTreesII.generateTrees(2);
+    List<TreeNode> treeNodes = uniqueBinarySearchTreesII.generateTrees(20);
     for (TreeNode root : treeNodes) {
       System.out.println(new BinaryTree(root));
     }
