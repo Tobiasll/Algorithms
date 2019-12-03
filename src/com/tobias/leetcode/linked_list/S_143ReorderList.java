156603ce97bb81a26cb987db622f565e85d2d598
Tobaisll <2289975919@qq.com>
2019-12-03 10:49:34 +0800

    ` `

--- src/com/tobias/leetcode/linked_list/S_143ReorderList.java
+++ src/com/tobias/leetcode/linked_list/S_143ReorderList.java
@@ -1,6 +1,9 @@
 package com.tobias.leetcode.linked_list;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Given a singly linked list L: L0→L1→…→Ln-1→Ln,
  * reorder it to: L0→Ln→L1→Ln-1→L2→Ln-2→…
@@ -17,11 +20,27 @@ package com.tobias.leetcode.linked_list;
 public class S_143ReorderList {
 
 
-  /**
-   * Runtime: 755 ms, faster than 5.02% of Java online submissions for Reorder List.
-   * Memory Usage: 38.9 MB, less than 100.00% of Java online submissions for Reorder List.
-   */
   public void reorderList(ListNode head) {
+    List<ListNode> list = new ArrayList<>();
+    while (head != null) {
+      list.add(head);
+      head = head.next;
+    }
+    int i = 0;
+
+    for (int j = list.size() - 1; i < j; j--) {
+      list.get(i).next = list.get(j);
+      i++;
+      list.get(j).next = list.get(i);
+    }
+    list.get(i).next = null;
+  }
+
+    /**
+     * Runtime: 755 ms, faster than 5.02% of Java online submissions for Reorder List.
+     * Memory Usage: 38.9 MB, less than 100.00% of Java online submissions for Reorder List.
+     */
+  public void reorderList1(ListNode head) {
 
     ListNode curRoot = head;
     while (curRoot != null && curRoot.next != null && curRoot.next.next != null) {
@@ -41,8 +60,8 @@ public class S_143ReorderList {
 
   public static void main(String[] args) {
     S_143ReorderList reorderList = new S_143ReorderList();
-//    ListNode listNode = new ListNode(new int[]{1, 2, 3, 4});
-    ListNode listNode = new ListNode(new int[]{1, 2});
+    ListNode listNode = new ListNode(new int[]{1, 2, 3, 4, 5});
+//    ListNode listNode = new ListNode(new int[]{1, 2});
     reorderList.reorderList(listNode);
     System.out.println(listNode);
   }
