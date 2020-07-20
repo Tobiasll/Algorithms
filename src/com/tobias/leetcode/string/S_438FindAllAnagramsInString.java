7bb287d7c0b39da2846fc0fee56816edba449ce6
Tobiasll <2289975919@qq.com>
2020-07-20 14:09:07 +0800

    ‘’

--- src/com/tobias/leetcode/string/S_438FindAllAnagramsInString.java
+++ src/com/tobias/leetcode/string/S_438FindAllAnagramsInString.java
@@ -1,6 +1,7 @@
 package com.tobias.leetcode.string;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -36,12 +37,107 @@ import java.util.List;
  */
 public class S_438FindAllAnagramsInString {
 
-    public List<Integer> findAnagrams(String s, String p) {
 
-        return null;
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> result = new ArrayList<>();
+
+        if (s == null || p == null || s.length() == 0 || p.length() == 0 || p.length() > s.length()) {
+            return result;
+        }
+
+        int[] pCounts = new int[26], sCounts = new int[26];
+        int left = 0, right = 0;
+        for (int i = 0; i < p.length(); i++) {
+            pCounts[p.charAt(i) - 'a']++;
+        }
+
+        while (right < p.length()) {
+            sCounts[s.charAt(right++) - 'a']++;
+        }
+        while (right < s.length()) {
+            if (judge(pCounts, sCounts)) {
+                result.add(left);
+            }
+            sCounts[s.charAt(left) - 'a']--;
+            sCounts[s.charAt(right) - 'a']++;
+            left++;
+            right++;
+        }
+        if (judge(pCounts, sCounts)) {
+            result.add(left);
+        }
+        return result;
+    }
+
+    private boolean judge(int[] pCounts, int[] sCounts) {
+        for (int i = 0; i < pCounts.length; i++) {
+            if (pCounts[i] != sCounts[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Runtime: 2603 ms, faster than 5.01% of Java online submissions for Find All Anagrams in a String.
+     * Memory Usage: 52.5 MB, less than 5.04% of Java online submissions for Find All Anagrams in a String.
+     */
+    public List<Integer> findAnagrams1(String s, String p) {
+        List<Integer> result = new ArrayList<>();
+
+        if (p == null || p.length() == 0) {
+            return result;
+        }
+
+        if (p.length() == 1) {
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) == p.charAt(0)) {
+                    result.add(i);
+                }
+            }
+            return result;
+        }
+
+        int[] counts = help(p);
+
+        for (int i = 0; i < s.length(); i++) {
+            if (counts[s.charAt(i) - 'a'] > 0) {
+                counts[s.charAt(i) - 'a']--;
+                int match = p.length() - 1;
+                for (int j = i + 1; j < s.length(); j++) {
+                    if (counts[s.charAt(j) - 'a'] > 0) {
+                        match--;
+                        counts[s.charAt(j) - 'a']--;
+                    } else {
+                        counts = help(p);
+                        break;
+                    }
+                    if (match == 0) {
+                        result.add(i);
+                        counts = help(p);
+                        break;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    private int[] help(String p) {
+        int[] counts = new int[26];
+
+        for (int i = 0; i < p.length(); i++) {
+            counts[p.charAt(i) - 'a']++;
+        }
+        return counts;
     }
 
     public static void main(String[] args) {
-
+        S_438FindAllAnagramsInString findAllAnagramsInString = new S_438FindAllAnagramsInString();
+        System.out.println(findAllAnagramsInString.findAnagrams("aaaaaaaaaa", "aaaaaaaaaaaaa"));
+        System.out.println(findAllAnagramsInString.findAnagrams("cbaebabacd", "abc"));
+        System.out.println(findAllAnagramsInString.findAnagrams("", "a"));
+        System.out.println(findAllAnagramsInString.findAnagrams("abab", "ab"));
+        System.out.println(findAllAnagramsInString.findAnagrams("acdcaeccde","c"));
     }
 }
